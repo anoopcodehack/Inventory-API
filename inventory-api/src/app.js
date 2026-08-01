900de8b6e298f5c5901f662329e4aea05f290b7d
Anoop-A <anooparjun05@gmail.com>
2026-08-01 20:43:03 +0530

    "feat: Project setup & folder structure "

---
+++ inventory-api/src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Inventory API Running"
+  });
+});
+
+module.exports = app;
